77eda90372d1af8b2b041aaec33b00d9667bb69e
Alicia Guerra <alicia.developer@aliciaguerra.com>
2016-05-02 00:12:42 -0700

    Update waitnotifychat.java

--- waitnotifychat.java
+++ waitnotifychat.java
@@ -31,6 +31,16 @@ class Chat {
       
     class T1 implements Runnable{
       chat m;
+      String[] s1 = {"Hi", "How are you", "I am also doing fine"};
+      
+      public T1(Chat m1){
+         this.m=m1;
+         new Thread(this, "Question").start();
+      }
+      
+      public void run(){
+         
+      }
       
       
       
