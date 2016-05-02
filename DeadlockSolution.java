65dc8bdce2da8c85e86f971e00d7b859eff90309
Alicia Guerra <alicia.developer@aliciaguerra.com>
2016-05-02 09:58:32 -0700

    Update DeadlockSolution.java

--- DeadlockSolution.java
+++ DeadlockSolution.java
@@ -10,3 +10,16 @@ public class TestThread {
   }
   
   private static class ThreadDemo1 extends Thread {
+    public void run(){
+      synchronized(Lock1) {
+        System.out.println("Thread 1: Holding lock 1...");
+        try{Thread.sleep(10);}
+        catch(InterruptedException e){}
+        System.out.println("Thread 2: Waiting for lock 2...");
+        synchronized(Lock2){
+         System.out.println("Thread 2: locking 1 & 2");
+        }
+      }
+    }
+  }
+}
