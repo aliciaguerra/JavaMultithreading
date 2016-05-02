57e28d25dfc8d88a7430b53916f7e4879bb4ab47
Alicia Guerra <alicia.developer@aliciaguerra.com>
2016-05-02 00:54:18 -0700

    Update waitnotifychat.java

--- waitnotifychat.java
+++ waitnotifychat.java
@@ -39,8 +39,36 @@ class Chat {
       }
       
       public void run(){
+         for(int i=0; i<s1.length; i++) {
+            m.Question(s1[i]);
+           }
+          }
+        }
+        
+      class T2 implements Runnable {
+         Chat m;
+         String[] s2 = {"Hi", "What about you?", "Great!"};
          
-      }
+         public T2(Chat m2){
+            this.m=m2;
+            new Thread(this, "Answer").start();
+         }
+      
+         public void run(){
+            for(int i=0; i<s2.length(); i++){
+               m.Answer(s2[i]);
+             }
+            }
+         }  
+         
+         public class TestThread{
+            public static void main(String args[]){
+               Chat m=new Chat();
+               new T1(m);
+               new T2(m);
+            }
+         }
+
       
       
       
